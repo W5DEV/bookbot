eafd5bb4d7f2f3427caa5e00358c5f889094d3c5
John Larson <jl@buildmidwestern.com>
2024-06-18 00:23:27 -0500

    added character count

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
